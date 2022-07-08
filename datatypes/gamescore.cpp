eebcf3a0b92417a3feb6c77cbd4387981f8039f9
Magz8984 <collinsmuthinja@gmail.com>
2022-07-08 20:41:17 +0300

    Create game score function

---
+++ datatypes/gamescore.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+ Define a variable holding the score in an int type variable
+ Print the value
+*/
+int main() {
+    int gameScore = 20; // A variable to store the points acquired in the game
+    cout << gameScore; // Print it to the standard C++ output stream
+    return 0; // exit
+}

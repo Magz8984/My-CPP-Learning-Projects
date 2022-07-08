37aedb6cc8d9ac22a59a22d257d37de69d4da790
Magz8984 <collinsmuthinja@gmail.com>
2022-07-08 20:47:33 +0300

    Add booleans file

---
+++ datatypes/booleans.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+// Define a boolean function to hold a binary value eg 1 or 0
+int main() {
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << isCodingFun; // 1
+    cout << isFishTasty; // 0
+
+    return 0;
+}

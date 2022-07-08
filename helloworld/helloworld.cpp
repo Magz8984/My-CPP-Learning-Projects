009abe6a088b84476f451f44e4ea2a51246acdbc
Magz8984 <collinsmuthinja@gmail.com>
2022-07-08 20:18:09 +0300

    Add helloworld cpp file comments

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Compiler includes the standard C++ I/O library
+
+using namespace std; // allows the use of standard C++ I/O library without std:: prepened to its methods
+
+/*
+ * The main function of the app (Entry point simial to Java's public static void main) 
+ * Return type is int cause it returns an int
+ */
 int main() {
-  cout << "Hello World! \n";
-  return 0;
+  cout << "Hello World! \n"; // Prints hello world to standard output stream using Cout
+  return 0; // exit
 }

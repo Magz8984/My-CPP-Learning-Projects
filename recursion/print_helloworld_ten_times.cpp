473c08a3161097379e614db06d551bc16c155c89
Magz8984 <collinsmuthinja@gmail.com>
2022-07-09 19:08:00 +0300

    Add recursion file

---
+++ recursion/print_helloworld_ten_times.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Recursion to print hello n number of times iterativley
+ */ 
+void printHelloWorld(int length, int time) {
+    if(length > time) {
+        cout << ++time << " Hello World\n";
+        printHelloWorld(length, time);
+    }
+}
+
+
+// Add logic before recursive call to reverse print hello world from x => 1
+void printHelloWorldReverse(int length, int time) {
+    if(length > time) {
+     printHelloWorldReverse(length, ++time);
+     cout << (time) << " Hello World\n";
+    }
+    
+}
+
+
+int main (){
+    printHelloWorld(100, 0);
+    printHelloWorldReverse(100, 0);
+    return 0;
+}
+
+// Check recursion in true

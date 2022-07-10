599670b738e0def884cd5bb84dfe5c43eda2be1d
Magz8984 <collinsmuthinja@gmail.com>
2022-07-10 10:27:59 +0300

    Add string functions file

---
+++ strings/string_functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h> // Include string header file
+using namespace std;
+
+
+int main () {
+    char name[50];
+
+    char name2[50] = "Muthinja";
+
+    // Copy value on right cariable to left
+    strcpy(name, "Collins");
+    
+    cout << name << "\n";
+
+    // Compare two strings and return true if is equal
+    int isEqual = strcmp(name, "Collins");
+
+    cout << "Is equal\t" << isEqual << "\n";
+
+    // Concatenate two strings
+    strcat(name, name2);
+    
+    cout << "New name is " << name << "\n";
+
+    // Check the string length
+    int strLength = strlen(name);
+
+    cout << name << " has " << strLength << " characters " << "\n";
+
+    return 0;
+}
